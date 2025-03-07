fba617e1bed0471e985f0225d38dba34ad72913e
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-07 06:56:44 -0300

    bloco-fonte  07/03/2025

---
+++ bloco.js
@@ -0,0 +1,15 @@
+
+let preco = 19.90;
+let desconto = 0.4;
+console.log(19.90 * 0.6);
+console.log("******************")
+console.log("preco:"+preco);//console.log("preco:", preco);
+var a = 20;
+var b = 1.5;
+var c =  a + b;
+{
+
+    console.log('passo1');
+    console.log(c);
+    
+}
