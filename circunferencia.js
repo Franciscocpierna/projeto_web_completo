7c89f69f419a4d7d6325814b9eeefedead8bef1e
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-07 18:03:59 -0300

    circunferencia 07/03/2025

---
+++ circunferencia.js
@@ -0,0 +1,6 @@
+const PI = 3.14159265358979323846;
+raio = 10;
+let circunferencia =  PI * raio * raio;
+console.log("o valor da area é "+circunferencia.toFixed(2));
+console.log("o vqalor de PI é ", Math.PI.toFixed(2));
+console.log("o valor de PI é ", Math.PI);
---
+++ tipodados.js
@@ -0,0 +1,19 @@
+
+const a = 10;
+let idade = 31;
+console.log(idade);
+console.log(typeof idade);
+let salario = 4578.32;
+console.log(salario);
+console.log(typeof salario);
+let estaChovendo = true;
+console.log(estaChovendo);
+console.log(typeof estaChovendo);
+console.log(typeof 'Teste');
+console.log(typeof "Teste");
+console.log(typeof `Teste`);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1);
+console.log(typeof 1.5);
+console.log(typeof -1);
