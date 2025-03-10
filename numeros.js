af0e9fd041ac466b940a8f4e50694e4364889285
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-10 12:29:57 -0300

    numeros 10/03/2025

--- numeros.js
+++ numeros.js
@@ -10,7 +10,15 @@ const media = total / (peso + peso2)
 console.log(media)
 console.log("******************")
 console.log(media.toFixed(2))
-console.log(media.toString())
+console.log(media.toString()) // em string
 console.log(media.toString(2)) // em binário
 console.log(typeof media) // Number
 console.log(typeof Number) // Number é uma função
+console.log("10" / 2) // 5
+console.log("Show!" * 2) // NaN
+console.log(0.1 + 0.7) // 0.7999999999999999
+// console.log(10.toString()) // não funciona
+console.log((10).toString()) // funciona
+console.log(typeof((10).toString())) // string
+console.log(7 / 0) // Infinity
+console.log((0.1 + 0.7).toFixed(2)) // 0.8
