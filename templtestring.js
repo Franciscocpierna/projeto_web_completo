3d06b374517c85911a9c78c4fb21cd659a880b27
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-11 16:04:09 -0300

    templtestring 11/03/2024

---
+++ templtestring.js
@@ -0,0 +1,13 @@
+const nome = "Rebeca"
+const concatenacao = "Olá " + nome + "!"
+const template = `
+    Olá
+    ${nome}!`
+console.log(concatenacao, template)
+console.log("*********")
+// Expressões...
+console.log(`1 + 1 = ${1 + 1}`)
+console.log("*********")
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up("cuidado")}!`)
+console.log("*********")
