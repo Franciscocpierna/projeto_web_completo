877db24b8f6c610d7a4909197c57cc019b610e91
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-15 14:31:02 -0300

    usando let var

---
+++ fundamentos/usando_var_loop.js
@@ -0,0 +1,11 @@
+for(var i = 0; i < 10; i++) {
+    console.log(i)
+}
+console.log("valor de i fora do for = "+ i)
+
+// usando let no loop
+
+for(let j = 0; j < 10; j++) {
+    console.log(j)
+}
+console.log("let fora do for = "+ j)
