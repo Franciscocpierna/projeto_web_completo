9d448119d673c7eb06fc978e66c5eec74163403d
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-17 14:14:03 -0300

    fundamentos/Hoisting 17/03/2025

--- fundamentos/usando_var_loop.js
+++ fundamentos/usando_var_loop.js
@@ -1,4 +1,4 @@
-for(var i = 0; i < 10; i++) {
+/*for(var i = 0; i < 10; i++) {
     console.log(i)
 }
 console.log("valor de i fora do for = "+ i)
@@ -8,4 +8,27 @@ console.log("valor de i fora do for = "+ i)
 for(let j = 0; j < 10; j++) {
     console.log(j)
 }
-console.log("let fora do for = "+ j)
+console.log("let fora do for = "+ j) */
+
+
+
+
+const funcs = []   //com let mostra o valor exato de i
+for (let i=0;i<10;i++){
+    funcs.push(function(){
+       console.log(i)
+
+    })
+}
+funcs[2]()
+funcs[8]()
+// com var mostra 10 sempre
+const funcs1 = []
+for (var i=0;i<10;i++){
+    funcs1.push(function(){
+       console.log(i)
+
+    })
+}
+funcs1[4]()
+funcs1[8]()
