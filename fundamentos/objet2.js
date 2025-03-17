4cbb7431f7e4b4f1d43302ed1df874ebac26b770
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-17 14:36:47 -0300

    object2 17/03/2025

---
+++ fundamentos/objet2.js
@@ -0,0 +1,12 @@
+console.log(typeof Object)//saída function
+console.log(typeof new Object)// saida object
+
+const Cliente = function(){}
+console.log(typeof Cliente) // saida function
+console.log(typeof new Cliente) //saida object
+
+class Produto {}
+console.log(typeof Produto) // saida function
+console.log(typeof new Produto()) // saida object
+
+
