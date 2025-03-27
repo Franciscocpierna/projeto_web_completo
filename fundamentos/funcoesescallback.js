1664671169d16e4d7c30575c3a3c177163f5961b
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-27 15:43:05 -0300

    funcoesescallback 27/03/2025

--- fundamentos/funcoesescallback.js
+++ fundamentos/funcoesescallback.js
@@ -9,3 +9,33 @@ fabricante.forEach(imprimir)
 fabricante.forEach(fabricante => console.log(fabricante))
 
 console.log("Funções Callback #02")
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+//sem callback
+//quatro resultados abaixo são iguais
+let notasBaixas = []
+for(let i in notas){
+    if(notas[i] < 7){
+        notasBaixas.push(notas[i])
+    }
+    //console.log(notasBaixas) 
+}
+console.log(notasBaixas)
+notasBaixas = notas.filter(function (nota){
+    return nota < 7
+})
+console.log(notasBaixas)
+const notasBaixas3 = notas.filter(nota => nota<7)
+console.log(notasBaixas3)
+
+const notasMenorque7 = nota => nota<7
+const notasBaixas4 = notas.filter(notasMenorque7)
+console.log(notasBaixas4)
+
+
+console.log("Funções Callback #03")
+/*
+executa no browse
+document getElementsByTagName('body')[0].onclick = function  (e)  {
+      console.log("O evento ocorreu")
+}
+*/ 
