11b0648152605e98f7f5cd84405fc66f720353b8
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-03-28 14:31:01 -0300

    funcao_tipos_declaracao e construtora   funcao_contexto_lexico 28/03/2025

---
+++ fundamentos/funcao_tipos_declaracao.js
@@ -0,0 +1,16 @@
+console.log(soma(3, 4))
+
+//função declaração
+function soma(x,y){
+    return x + y
+}
+
+//function expression
+const sub = function (x, y){
+    return x - y
+}
+// named funtion expression
+const mult = function mult(x, y) {
+    return x * y
+}
+
