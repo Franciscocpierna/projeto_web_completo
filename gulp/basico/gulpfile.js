a2f24870d9be42963c06e03e5ed5514c2dfff599
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-06-18 12:06:05 -0300

    gulpfile   18/06/2025

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,9 +1,32 @@
 const gulp = require('gulp')
-const series = gulp.series
-function copiar(cb) {
-    console.log('Tarefa de copiar!')
+const {series, parallel} = require('gulp')
+
+
+
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!')
     return cb() 
 }
 
+const antes2 = cb  =>  {
+    console.log('Tarefa Antes 2!')
+    return cb() 
+}
 
-module.exports.default = series(copiar)
+function copiar(cb) {
+    //gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+     gulp.src('pastaA/**/*.txt')
+                .pipe(gulp.dest('pastaB'))
+     return cb() 
+}
+
+const fim = cb  =>  {
+    console.log('Tarefa Fim!')
+    return cb() 
+}
+
+module.exports.default = series(
+    parallel(antes1,antes2),
+    copiar,
+    fim,
+)
