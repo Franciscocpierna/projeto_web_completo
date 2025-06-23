43f7e0525a916e961aba0b881b82e6f3882801b7
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-06-23 09:21:44 -0300

    spa  23/06/2025

--- gulp/spa/gulptasks/deps.js
+++ gulp/spa/gulptasks/deps.js
@@ -6,12 +6,12 @@ function depsCSS(cb)  {
   return gulp.src('node_modules/font-awesome/css/font-awesome.css')  
          .pipe(uglifycss({ "uglyComments" : false })) 
          .pipe(concat('deps.min.css')) 
-         .pipe(gulp.dest('buid/assets/css')) 
+         .pipe(gulp.dest('build/assets/css')) 
 }
 
 function depsFonts()  {
     return gulp.src('node_modules/font-awesome/fonts/*.*') 
-           .pipe(gulp.dest('buid/assets/fonts'))
+           .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
