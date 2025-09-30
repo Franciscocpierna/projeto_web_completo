77d090f11434f16c36efd8532b6ebfe4ba928176
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-09-30 19:27:59 -0300

    projeto calculadora 30/09/2025

---
+++ vue/calculator/src/components/Display.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="display">
+        {{ value }}
+    </div>
+</template>
+
+<script>
+export default {
+    // 🛑 CORREÇÃO: Adicionando um nome multi-palavra para satisfazer o ESLint.
+    name: 'CalcDisplay', 
+    props: ['value']
+}
+</script>
+
+<style>
+    .display {
+        grid-column: span 4;
+        background-color: #0004;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 20px;
+        font-size: 2.1em;
+        overflow: hidden;
+    }
+</style>
