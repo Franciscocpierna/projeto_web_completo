9398d4d60af7dcc7ecf03c49b86690479f45f19f
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-10-27 15:14:45 -0300

    Backend_Frontend  27/10/2025

--- Backend _Frontend/versao-final/backend/knexfile.js
+++ Backend _Frontend/versao-final/backend/knexfile.js
@@ -2,7 +2,12 @@ const { db } = require('./.env')
 
 module.exports = {
 	client: 'postgresql',
-	connection: db,
+	//connection: db,
+	connection: {
+		database: 'Knowledge_final',
+		user: 'postgres',
+		password: 'pierna'
+	},
 	pool: {
 		min: 2,
 		max: 10
