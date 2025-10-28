733045f0e8cc928749c354f3547bd718b61ca02d
Francisco Carlos Pierna <pierna_f@hotmail.com>
2025-10-28 16:01:05 -0300

    backend frontend  28/10/2025

--- Backend_Frontend/knowledge-master/versao-final/backend/knexfile.js
+++ Backend_Frontend/knowledge-master/versao-final/backend/knexfile.js
@@ -11,3 +11,23 @@ module.exports = {
 		tableName: 'knex_migrations'
 	}
 };
+
+/*
+const { db } = require('./.env')
+
+module.exports = {
+	client: 'postgresql',
+	connection:{
+		database: 'knowledge_final',
+		user: 'postgres',
+		password: '123456' 
+	},	
+	pool: {
+		min: 2,
+		max: 10
+	},
+	migrations: {
+		tableName: 'knex_migrations'
+	}
+};
+*/
